4a4380f7f440758a317cad02521cc1c29a2975cc
ilkerCelimli <ilkerCelimli@outlook.com>
2023-06-01 11:43:05 +0300

    a

--- survey-service/src/main/java/com/surveywizz/surveyservice/entity/Requirements.java
+++ survey-service/src/main/java/com/surveywizz/surveyservice/entity/Requirements.java
@@ -2,6 +2,8 @@ package com.surveywizz.surveyservice.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Requirements extends BaseEntity {
